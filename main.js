b4dad287f71c71456a4711f96467bdb3ab65e848
Jolann <jmadec@enseirb-matmeca.fr>
2025-02-21 20:29:20 +0100

    V3

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 import fetch from 'node-fetch';
 import nodeNotifier from 'node-notifier';
 
+const H = '7ed655e362544364aefb2fb91081bc53bdd33929';
+const cid = "2027860180";
+const sid = "w0oo4g8cg4ws4c40gwokwgsc4wss0ow84kgwccg84o48s8g0804wcws4wc0gswos";
+const SECONDS = 1000;
+
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
-
-const H = 'd47a1196dcb8ec2623ea94283054054261e9b128';
-const cid = "887926458";
-const sid = "4cgk804ksgcwg4k80o8c8k0g004kggsw4ok80k8c4c4c4ccks4k4ggc0skw8wsow"
-const referer = "https://fr166.grepolis.com/game/index?login=1&p=849068155&ts=1731333660"
-const SECONDS = 1000;
 
 function checkGold(data) {
   if (data.json) {
@@ -19,21 +18,21 @@ function checkGold(data) {
     const stoneDiff = data.json.stone.capacity - data.json.stone.stock;
     const ironDiff = data.json.iron.capacity - data.json.iron.stock;
 
-    if (woodDiff > 500) {
+    if (woodDiff > 1000) {
       nodeNotifier.notify({
         title: `OR mer ${data.json.sea_id}`,
         message: `Trou de ${woodDiff} de bois`
       });
     }
   
-    if (stoneDiff > 500) {
+    if (stoneDiff > 1000) {
       nodeNotifier.notify({
         title: `OR mer ${data.json.sea_id}`,
         message: `Trou de ${stoneDiff} de pierre`
       });
     }
   
-    if (ironDiff > 500) {
+    if (ironDiff > 1000) {
       nodeNotifier.notify({
         title: `OR mer ${data.json.sea_id}`,
         message: `Trou de ${ironDiff} d'argent`
@@ -45,12 +44,11 @@ function checkGold(data) {
 }
 
 async function fetchTownData(townId) {
-  const response = await fetch(`https://fr166.grepolis.com/game/frontend_bridge?town_id=${townId}&action=execute&h=${H}&json={"model_url":"PremiumExchange","action_name":"read","town_id":${townId},"nl_init":true}`, {
+  const response = await fetch(`https://fr170.grepolis.com/game/frontend_bridge?town_id=${townId}&action=execute&h=${H}&json={"model_url":"PremiumExchange","action_name":"read","town_id":${townId},"nl_init":true}`, {
     headers: {
       "Content-Type": "application/json",
       "x-requested-with": "XMLHttpRequest",
-      "cookie": `cid=${cid}; sid=${sid}; toid=${townId};`,
-      "Referer": referer,
+      "cookie": `cid=${cid}; sid=${sid}; toid=${townId};`
     }
   });
   if (!response.ok) {
@@ -63,12 +61,12 @@ async function start() {
   try {
     console.log("Recherche de gold...");
 
-    const dataTown = await fetchTownData(429);
+    const dataTown = await fetchTownData(510);
     console.log(`Récupération des données pour la ville ${dataTown.json.sea_id}...`);
     checkGold(dataTown);
 
     console.log("Attente de 5 secondes...");
-    await sleep(5 * SECONDS);
+    await sleep(30 * SECONDS);
     start();
   } catch (error) {
     console.error("Erreur dans le processus:", error);
